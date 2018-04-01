10a9be4e84394f59deec20241c0d0f9710d23422
Jimskapt <rami.programmer@gmail.com>
2018-04-01 12:09:21 +0200

    Displaying version in header.

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <v-app>
     <v-toolbar dark color="primary">
       <v-toolbar-title>
-        <span>Ikiliptus</span>
+        <span>Ikiliptus <sub style="font-size:8px;font-style:italic;">{{release}}</sub></span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
         <v-btn icon v-bind:to="{name: 'LiveCounter'}">
@@ -17,7 +17,17 @@
 </template>
 
 <script>
+import Package from '../package.json'
+
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      release: '0.0.0'
+    }
+  },
+  mounted () {
+    this.release = Package.version
+  }
 }
 </script>
