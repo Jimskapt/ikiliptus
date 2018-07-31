b9ecf3e3c9d681602f2262acaae2fbfb9c134009
Jimskapt <rami.programmer@gmail.com>
2018-07-31 07:20:32 +0200

    Fixing i18n errors.

--- src/i18n.ts
+++ src/i18n.ts
@@ -4,10 +4,10 @@ import VueI18n, { LocaleMessages } from 'vue-i18n';
 Vue.use(VueI18n);
 
 function loadLocaleMessages(): LocaleMessages {
-  const locales = require.context('./locales', true, /[a-z0-9]+\.json$/i);
+  const locales = require.context('./locales', true, /[a-z0-9\-]+\.json$/i);
   const messages: LocaleMessages = {};
   locales.keys().forEach((key) => {
-    const matched = key.match(/([a-z0-9]+)\./i);
+    const matched = key.match(/([a-z0-9\-]+)\./i);
     if (matched && matched.length > 1) {
       const locale = matched[1];
       messages[locale] = locales(key);
